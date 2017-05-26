3406a390f2072b595736ff0103c420e40db3846a
HC <huangchun_cd@keruyun.com>
2017-05-26 16:23:28 +0800

    add redux devtool with browser extension

--- src/store/create-store.js
+++ src/store/create-store.js
@@ -2,7 +2,8 @@ import { createStore } from 'redux';
 import rootReducer from '../reducer/index';
 
 export default function(data) {
-	let store = createStore(rootReducer, data);
+	let store = createStore(rootReducer, data,
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
  
 	if (module.hot) {
 		module.hot.accept('../reducer', () => {

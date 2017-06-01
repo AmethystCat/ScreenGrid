a62e2d77eac7fe80b6550f56bb16d4c10b5148a2
HC <lovelifeloveyoumore@gmail.com>
2017-06-02 06:56:49 +0800

    add redux thunk and store config updated

--- src/store/create-store.js
+++ src/store/create-store.js
@@ -1,9 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducer/index';
 
-export default function(data) {
-	let store = createStore(rootReducer, data,
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// const enhancer = () => {
+// 	 compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// };
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default function(initialState) {
+	let store = createStore(rootReducer, initialState, composeEnhancers(
+			applyMiddleware(thunk)
+		));
  
 	if (module.hot) {
 		module.hot.accept('../reducer', () => {

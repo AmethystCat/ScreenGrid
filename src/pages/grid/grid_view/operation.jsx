8c2d866fd5131714435884c8a5056b7d5ea571b7
HC <huangchun_cd@keruyun.com>
2017-06-10 14:10:18 +0800

    add operation

---
+++ src/pages/grid/grid_view/operation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+export class Operation extends React.Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default connect()(Operation);

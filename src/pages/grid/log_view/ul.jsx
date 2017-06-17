b7d63c791bb38322c154c85efb0bbeba49b75f5b
HC <huangchun_cd@keruyun.com>
2017-06-17 17:50:13 +0800

    auido log updated

--- src/pages/grid/log_view/ul.jsx
+++ src/pages/grid/log_view/ul.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Ul(props) {
     return (<ul>
         {props.list.map((log, index) => {
-            return <li title={log.description} key={index}>{log.matrixPort}{log.description}{log.toMatrixPort || ''}</li>;
+            return <li title={log.description} key={index}>{log.matrixPort}{log.description}</li>;
         })}
     </ul>);
 }

53e088704e665d2b215c5792e0f8deb7f123ef4d
HC <huangchun_cd@keruyun.com>
2017-06-17 19:33:42 +0800

    getNewConnection logic changed

--- src/common/helper.js
+++ src/common/helper.js
@@ -1,13 +1,22 @@
 export const getNewConnections = (originConnections = [], requestConnectionObj = {}) => {
     let req_inPortId = requestConnectionObj.inPortId,
         req_outPortId = requestConnectionObj.outPortId;
+    let newConnections = originConnections.slice(0);
 
+    // 过滤是否含有相同的坐标
     let filterConnections = originConnections.filter((connect) => {
-        return (connect.inPortId === req_inPortId && connect.outPortId === req_outPortId);
+        return (connect.inMatrixPort === req_inPortId && connect.outMatrixPort === req_outPortId);
     });
+    // 如果含有相同的坐标（filterConnections.length > 0），表示要将该方块点灭，将该坐标从原数组中移除
+    if (filterConnections.length) {
+        newConnections = originConnections.filter((connect) => {
+            return !(connect.inMatrixPort === filterConnections[0].inMatrixPort && connect.outMatrixPort === filterConnections[0].outMatrixPort);
+        });
+        return newConnections;
+    };
+    // 反之，如果filterConnections.length == 0， 表示该方块为点亮，要存进原数组并返回
+    newConnections.push({inMatrixPort: req_inPortId, outMatrixPort: req_outPortId});
 
-    if (originConnections.length > filterConnections.length) return filterConnections;
-    let newConnections = originConnections.push({inPortId: req_inPortId, outPortId: req_outPortId});
     return newConnections;
 };
 

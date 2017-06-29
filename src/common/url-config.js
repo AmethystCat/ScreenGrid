5a14477722fe98f49cc4c5f8fd822dc80ee27220
HC <lovelifeloveyoumore@gmail.com>
2017-06-30 06:12:47 +0800

    remove video url config

--- src/common/url-config.js
+++ src/common/url-config.js
@@ -1,23 +1,13 @@
-let video = {
-		sceneListUrl: '/icmc/systemManage/scene/sceneList',
-		getListBySceneUrl: '/icmc/systemManage/videoMatrix/getListByScene',
-		getListBySceneAndTypeUrl: '/icmc/systemManage/videoMatrixPort/getListBySceneAndType',
-		connectionsUrl: '/icmc/operation/videoMatrixOperation/connections',
-		inToOutConnectUrl: '/icmc/operation/videoMatrixOperation/inToOutConnect',
-		connectionByOutUrl: '/icmc/operation/videoMatrixOperation/connectionByOut',
-		get15MatrixLogsUrl: '/icmc/internal/matrixLog/get15MatrixLogs'
-	};
-	
 let audio = {
 		sceneListUrl: '/icmc/systemManage/scene/sceneList',
 		getListBySceneUrl: '/icmc/systemManage/audioMatrix/getListByScene',
 		getListBySceneAndTypeUrl: '/icmc/systemManage/audioMatrixPort/getListBySceneAndType',
 		connectionsUrl: '/icmc/operation/audioMatrixOperation/connections',
 		inToOutConnectUrl: '/icmc/operation/audioMatrixOperation/inToOutConnect',
-		nToOutDisconnectUrl: '/icmc/operation/audioMatrixOperation/inToOutDisconnect',
+		inToOutDisconnectUrl: '/icmc/operation/audioMatrixOperation/inToOutDisconnect',
 		changeMuteUrl: '/icmc/operation/audioMatrixOperation/changeMute',
 		changeVolumeUrl: '/icmc/operation/audioMatrixOperation/changeVolume',
 		connectionByOutUrl: '/icmc/operation/audioMatrixOperation/connectionByOut',
 		get15MatrixLogsUrl: '/icmc/internal/matrixLog/get15MatrixLogs'
 	};
-export { video, audio };
+export { audio };

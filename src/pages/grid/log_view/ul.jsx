1d9a7c090a85a08b9b7ef37fe6f75b0e2f08ffc6
HC <huangchun_cd@keruyun.com>
2017-08-10 18:09:50 +0800

    log updated

--- src/pages/grid/log_view/ul.jsx
+++ src/pages/grid/log_view/ul.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 function Ul(props) {
     return (<ul>
         {props.list.map((log, index) => {
-            return <li title={log.description} key={index}>{log.matrixPort}{log.description}{log.toMatrixPort || ''}</li>;
+            return <li title={log.matrixPort + log.description + (log.toMatrixPort || '')} key={index}>
+				<span>
+					{index+1}. {log.matrixPort}
+					<span className="description">{log.description}</span>
+					{log.toMatrixPort || ''}
+				</span>
+			</li>;
         })}
     </ul>);
 }
